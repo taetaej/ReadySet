5e738c2519e84edd848a29c78906bb49d6ba3208
Jia Shin <jia.shin@cj.net>
2026-05-28 17:53:52 +0900

    fix: use Bootstrap Icons markdown SVG (MIT license)

--- src/components/docs/DocsLayout.tsx
+++ src/components/docs/DocsLayout.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useEffect, useRef } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { ChevronRight, ChevronLeft, Search, ArrowLeft, Sun, Moon, LogOut, ArrowUp, Link, FileText, Check, ChevronDown } from 'lucide-react'
+import { ChevronRight, ChevronLeft, Search, ArrowLeft, Sun, Moon, LogOut, ArrowUp, Link, Copy, Check, ChevronDown } from 'lucide-react'
 import { docsStructure, DocPage } from './docsData'
 import { Avatar } from '../common/Avatar'
 import { getDarkMode, setDarkMode } from '../../utils/theme'
@@ -579,7 +579,10 @@ export function DocsLayout({ isDarkMode: propDarkMode, onToggleDarkMode: propTog
               >
                 {copied === 'md' ? <Check size={14} /> : (
                   <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                    <path d="M2 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H2zm.5 1.5h2l1.5 2 1.5-2h2v5H8V6.5L6.5 8.5h-.025L5 6.5V9.5H3.5v-5h-1zm7 0h2v2l1.5-1.5v3l-1.5-1.5v2h-2v-4z"/>
+                    <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z"/>
+                    <path fillRule="evenodd" d="M9.146 8.146a.5.5 0 0 1 .708 0L11.5 9.793l1.646-1.647a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 0-.708z"/>
+                    <path fillRule="evenodd" d="M11.5 5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 1 .5-.5z"/>
+                    <path d="M3.56 11V7.01h.056l1.428 3.239h.774l1.42-3.24h.056V11h1.073V5.001h-1.2l-1.71 3.894h-.039l-1.71-3.894H2.5V11z"/>
                   </svg>
                 )}
                 <span>{copied === 'md' ? 'Copied' : 'Copy as Markdown'}</span>
